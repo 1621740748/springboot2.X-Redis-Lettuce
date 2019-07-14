cd727e38c4a02e157bfb0fa352f79d586b5c27a3
huangyan <huangyan@huangyan-PC>
2019-07-14 12:48:11 +0800

    提交

--- src/main/java/com/wuhao/redis/controller/UserController.java
+++ src/main/java/com/wuhao/redis/controller/UserController.java
@@ -1,11 +1,7 @@
 package com.wuhao.redis.controller;
 
-import com.wuhao.redis.model.ApiModel;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 
 /**
